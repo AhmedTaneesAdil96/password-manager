58b072f989396bc3338b0e105e41c544bf2c7f72
Ahmed Tanees Adil <ahmedtaneesadil@gmail.com>
2025-06-29 16:32:21 +0500

    chawas

--- src/app/api/passwords/reveal/route.ts
+++ src/app/api/passwords/reveal/route.ts
@@ -2,18 +2,19 @@ import { decrypt } from "../../../lib/encryption";
 import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../../lib/auth";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function POST(req: { json: () => PromiseLike<{ id: number }> | { id: number } }) {
+export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
-  if (!session) return new Response("Unauthorized", { status: 401 });
+  if (!session) return new NextResponse("Unauthorized", { status: 401 });
 
   const { id } = await req.json();
   const password = await prisma.password.findUnique({ where: { id } });
 
-  if (!password) return new Response("Not found", { status: 404 });
-  if (password.userId !== session.user?.id) return new Response("Forbidden", { status: 403 });
+  if (!password) return new NextResponse("Not found", { status: 404 });
+  if (password.userId !== parseInt(session.user?.id)) return new NextResponse("Forbidden", { status: 403 });
 
-  return Response.json({ password: decrypt(password.encryptedPwd) });
+  return NextResponse.json({ password: decrypt(password.encryptedPwd) });
 }
